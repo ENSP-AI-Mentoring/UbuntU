09a63ca7ddbb96c4b6d4a77c42f8be7300dfd984
junior.nnane <junior.nnane.ext@oscaro.com>
2023-07-09 15:43:37 +0200

    select 03 emails

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ random.seed(seed + salt)
 np.random.seed(seed + salt)
 
 
+
 def load_email_list(file_path):
     try:
         emails = pd.read_csv(file_path)
@@ -26,6 +27,7 @@ def exclude_emails(email_list, excluded_emails):
     email_list_usable = [email for email in email_list if email not in excluded_emails]
     n = len(email_list_usable)
     print(f"There are {n} usable candidates out of {len(email_list)} total.")
+    # Uniform probability distribution
     email_weights = [0 if email in excluded_emails else 1 / n for email in email_list]
     return email_weights
 
@@ -34,6 +36,20 @@ def select_random_emails(email_list, email_weights, num_emails):
     random_emails = np.random.choice(a=email_list, p=email_weights, size=num_emails, replace=False)
     return random_emails
 
+def mask_email(email):
+    # Split the email address into username and domain parts
+    username, domain = email.split('@')
+
+    # Calculate the number of characters to mask
+    mask_length = int(len(username) * 0.68)
+
+    # Mask the characters after the specified length
+    masked_username = username[:mask_length] + '*' * (len(username) - mask_length)
+
+    # Combine the masked username and domain to form the masked email
+    masked_email = masked_username + '@' + domain
+
+    return masked_email
 
 def main():
     try:
@@ -49,16 +65,17 @@ def main():
 
         # Assign a weight of 0 to the last n excluded emails
         print("Excluded emails are:")
+        #print([mask_email(email) for email in excluded_emails])
         print(excluded_emails)
 
         email_weights = exclude_emails(email_list, excluded_emails)
 
         # Sample three emails randomly based on the weights
-        num_emails_to_select = 2
+        num_emails_to_select = 3
         random_emails = select_random_emails(email_list, email_weights, num_emails_to_select)
 
         # Print the randomly chosen emails
-        print("Randomly chosen emails:")
+        print("Randomly",num_emails_to_select,"chosen emails:")
         for email in random_emails:
             print(email)
 
