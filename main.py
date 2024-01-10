98b811a49e97767d682a334e669f06474ea2a4d2
nprime496 <nnanejunior@gmail.com>
2024-01-10 22:56:12 +0100

    code refactoring

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 import random
 import numpy as np
 import pandas as pd  # type:ignore
-from typing import List, Dict
+from typing import List, Dict, Optional
 import re
-from datetime import date, datetime, timedelta
+from datetime import datetime, timedelta
 import os
+from dotenv import load_dotenv
+import logging
+import tempfile
+import argparse
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+load_dotenv()
 
 
-def convert_google_sheet_url(url):
+def convert_google_sheet_url(url: str) -> str:
     """
-        Function to convert Google Sheets URL into a CSV export URL
+    Function to convert Google Sheets URL into a CSV export URL
     """
     # Regular expression to match and capture the necessary part of the URL
-    pattern = r'https://docs\.google\.com/spreadsheets/d/([a-zA-Z0-9-_]+)(/edit#gid=(\d+)|/edit.*)?'
+    pattern = r"https://docs\.google\.com/spreadsheets/d/([a-zA-Z0-9-_]+)(/edit#gid=(\d+)|/edit.*)?"
 
     # Replace function to construct the new URL for CSV export
     # If gid is present in the URL, it includes it in the export URL, otherwise, it's omitted
-    replacement = lambda m: f'https://docs.google.com/spreadsheets/d/{m.group(1)}/export?' + (f'gid={m.group(3)}&' if m.group(3) else '') + 'format=csv'
+    def replacement(m: re.Match) -> str:
+        return (
+            f"https://docs.google.com/spreadsheets/d/{m.group(1)}/export?"
+            + (f"gid={m.group(3)}&" if m.group(3) else "")
+            + "format=csv"
+        )
 
     # Replace using regex
     new_url = re.sub(pattern, replacement, url)
@@ -24,23 +39,23 @@ def convert_google_sheet_url(url):
     return new_url
 
 
-def fetch_data(url, columns):
+def fetch_data_from_url(url: str, columns: List[str]) -> Optional[pd.DataFrame]:
     """
-        Function to fetch the information of the members
+    Function to fetch the information of the members
 
-        Args:
-        - url (str): CSV export URL of the Google Sheet containing the information to fetch
-        - columns (List of str): list containing the columns of the Google Sheet to return
+    Args:
+    - url (str): CSV export URL of the Google Sheet containing the information to fetch
+    - columns (List of str): list containing the columns of the Google Sheet to return
 
-        Returns:
-        - DataFrame containing the columns specified in argument
+    Returns:
+    - DataFrame containing the columns specified in argument
 
-        Raises:
-        - Exception: For any other unexpected errors during file reading.
+    Raises:
+    - Exception: For any other unexpected errors during file reading.
     """
     new_url = convert_google_sheet_url(url)
 
-    #read the data
+    # read the data
     try:
         df = pd.read_csv(new_url)
         return df[columns].dropna()
@@ -127,98 +142,156 @@ def select_random_emails(
     return random_emails.tolist()  # Converts numpy array to a Python list
 
 
-def mask_email(email: str) -> str:
-    """
-    Masks the characters in the username of an email address for privacy purposes.
+# def mask_email(email: str) -> str:
+#     """
+#     Masks the characters in the username of an email address for privacy purposes.
 
-    Args:
-    - email (str): The email address to be masked.
+#     Args:
+#     - email (str): The email address to be masked.
 
-    Returns:
-    - str: The masked email address with part of the username replaced by '*'.
-    """
+#     Returns:
+#     - str: The masked email address with part of the username replaced by '*'.
+#     """
 
-    # Split the email address into username and domain parts
-    username, domain = email.split("@")
+#     # Split the email address into username and domain parts
+#     username, domain = email.split("@")
 
-    # Calculate the number of characters to mask
-    mask_length = int(len(username) * 0.68)
+#     # Calculate the number of characters to mask
+#     mask_length = int(len(username) * 0.68)
 
-    # Mask the characters after the specified length
-    masked_username = username[:mask_length] + "*" * (len(username) - mask_length)
+#     # Mask the characters after the specified length
+#     masked_username = username[:mask_length] + "*" * (len(username) - mask_length)
 
-    # Combine the masked username and domain to form the masked email
-    masked_email = masked_username + "@" + domain
+#     # Combine the masked username and domain to form the masked email
+#     masked_email = masked_username + "@" + domain
 
-    return masked_email
+#     return masked_email
 
 
-def main() -> None:
+def main(args: Dict) -> None:
     """
     main method.
     """
     # Set the seed value based on a specific date
-    seed_date = "2023-07-15"  # Use the desired date
+    # seed_date = "2023-07-15"  # Use the desired date
     salt = 300
+
+    seed_date = args["seed_date"]
+    delay = args["delay"]
+    number_of_members_to_draw = args["number_of_members_to_draw"]
+    activities = args["activities"]
+    members_sheet_url = args["members_sheet_url"]
+    output_file = args["output_file"]
+    ACTIVITIES = ["presentation"]  # ["presentation", "article"]
 
     seed = int(datetime.now().timestamp())
     random.seed(seed + salt)
     np.random.seed(seed + salt)
 
-    DELAY = 3 # Number of months after which we are included in the draw again afer a participation
-    ACTIVITIES = ["presentation"] # ["presentation", "article"]
-    NB_OF_MEMBERS_TO_DRAW = 3
-
     # Get the information of the members
     # Raw URL of the Google Sheet containing the information of the members
-    members_sheet_url = str(os.environ.get('MEMBERS_SHEET_URL'))
 
     personal_info_columns = ["emails", "github username"]
 
-    participation_colmun_from_activity = lambda activity : f"{activity}_last_participation"
-    activity_drawable_column = lambda activity : f"{activity}_drawable"
+    def participation_colmun_from_activity(activity: str) -> str:
+        return f"{activity}_last_participation"
 
-    participation_columns = [participation_colmun_from_activity(activity) for activity in ACTIVITIES]
+    def activity_drawable_column(activity: str) -> str:
+        return f"{activity}_drawable"
 
-    data = fetch_data(members_sheet_url, personal_info_columns+participation_columns)
+    participation_columns = [
+        participation_colmun_from_activity(activity) for activity in ACTIVITIES
+    ]
 
-    def is_drawable(x):
+    data = fetch_data_from_url(
+        members_sheet_url, personal_info_columns + participation_columns
+    )
+
+    def is_drawable(date: str) -> bool:
         """
         Function to check if a person is drawable for the current draw
         """
-        try:
-            return (datetime.now() - datetime.strptime(x, "%d/%m/%Y")) >= timedelta(days=DELAY*30)
-        except Exception:
-            return True
+        return True
+        # try:
+        #     return (datetime.now() - datetime.strptime(date, "%d/%m/%Y")) >= timedelta(
+        #         days=DELAY * 30
+        #     )
+        # except Exception:
+        #     return True
 
-    if data is not None: # Fetching the data succeeded
-        for activity in ACTIVITIES:
-            #create  a column to indicate if we can draw each person or not for the activity
-            data[activity_drawable_column(activity)] = data[participation_colmun_from_activity(activity)].apply(lambda x: is_drawable(x))
+    if data is not None:  # Fetching the data succeeded
+        # for activity in ACTIVITIES:
+        # create  a column to indicate if we can draw each person or not for the activity
+        # data[activity_drawable_column(activity)] = data[
+        #     participation_colmun_from_activity(activity)
+        # ].apply(lambda x: bool(is_drawable(x)))
 
-            candidates_df = data[data[activity_drawable_column(activity)] == True]
+        candidates_df = data  # data[data[activity_drawable_column(activity)] == True]
 
-            email_list = candidates_df["emails"].values
+        logging.info(candidates_df)
 
-            # Shuffle the email list based on the seed value
-            random.shuffle(email_list)
+        email_list = candidates_df["emails"].values
 
-            # Choose the mails
-            if len(email_list) > NB_OF_MEMBERS_TO_DRAW:
-                choosed_emails = (np.random.choice(a=email_list, size=NB_OF_MEMBERS_TO_DRAW, replace=False)).tolist()
-            else:
-                choosed_emails = email_list
+        # Shuffle the email list based on the seed value
+        random.shuffle(email_list)
 
-            # Get the associated github usernames of the selected emails
-            selected_members = data[data["emails"].isin(choosed_emails)]
-            selected_members_gh_usernames = selected_members["github username"].values
+        # Choose the mails
+        if len(email_list) > number_of_members_to_draw:
+            choosed_emails = (
+                np.random.choice(
+                    a=email_list, size=number_of_members_to_draw, replace=False
+                )
+            ).tolist()
+        else:
+            choosed_emails = email_list
 
-            # Write the selected usernames in the GITHUB_ENV environment
-            env_file = os.getenv('GITHUB_ENV')
-        
-            with open(env_file, 'a') as file:
+        # Get the associated github usernames of the selected emails
+        selected_members = data[data["emails"].isin(choosed_emails)]
+        selected_members_gh_usernames = selected_members["github username"].values
+
+        # Write the selected usernames in the GITHUB_ENV environment
+        env_file = os.getenv("GITHUB_ENV")
+        if env_file is None:
+            # If GITHUB_ENV is not set, we are likely in dev env, use a temporary file
+            with tempfile.NamedTemporaryFile(mode="a", delete=False) as file:
                 for i, username in enumerate(selected_members_gh_usernames, start=1):
                     file.write(f"user{i}={username}\n")
-                
+        else:
+            with open(env_file, "a") as file_env:
+                for i, username in enumerate(selected_members_gh_usernames, start=1):
+                    file_env.write(f"user{i}={username}\n")
+    else:
+        logging.error("No data fetched ! Aborting.")
+
+
 if __name__ == "__main__":
-    main()
+    DELAY = 3  # Number of months after which we are included in the draw again afer a participation
+    NB_OF_MEMBERS_TO_DRAW = 1
+
+    parser = argparse.ArgumentParser(description="Member Selection Script")
+    parser.add_argument("--seed_date", type=str, help="Seed date for randomness")
+    parser.add_argument(
+        "--delay", type=int, default=3, help="Delay in months for re-inclusion in draw"
+    )
+    parser.add_argument(
+        "--number_of_members_to_draw",
+        type=int,
+        default=NB_OF_MEMBERS_TO_DRAW,
+        help="Number of members to draw",
+    )
+    parser.add_argument(
+        "--activities", nargs="+", default=["presentation"], help="List of activities"
+    )
+    parser.add_argument(
+        "--members_sheet_url",
+        type=str,
+        # required=True,
+        default=str(os.environ.get("MEMBERS_SHEET_URL")),
+        help="Google Sheet URL for member information",
+    )
+    parser.add_argument(
+        "--output_file", type=str, help="Output file for selected usernames"
+    )
+
+    args = vars(parser.parse_args())  # Converts to  dict
+    main(args)
